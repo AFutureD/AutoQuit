b8430917afa1aee92e3165e44bdacfd99433d5a5
Huanan <donghuanan@inston.ltd>
2025-09-23 19:00:05 +0800

    add TabView

--- AutoQuit/ContentView.swift
+++ AutoQuit/ContentView.swift
@@ -7,13 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum Tabs: Equatable, Hashable, Identifiable {
+    case general
+
+    var id: Int {
+        switch self {
+        case .general: 1
+        }
+    }
+}
+
+struct GeneralTabView: View {
     @EnvironmentObject var appState: AppState
 
     var body: some View {
         Button("Click Me") {
             appState.permissions.accessibility.checkPermissionAndPromptIfNeeded()
         }
+    }
+}
+
+
+struct ContentView: View {
+    @EnvironmentObject var appState: AppState
+
+    @State private var selectedTab: Tabs = .general
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Tab("General", systemImage: "gearshape.circle", value: .general) {
+                GeneralTabView()
+            }
+            .customizationID("general")
+        }
+        .tabViewStyle(.sidebarAdaptable)
     }
 }
 
